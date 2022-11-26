7d9c493fad3ae2caca3b58308fc85d4673c478bf
semanticism <67880895+semanticism@users.noreply.github.com>
2022-11-26 16:59:16 -0300

    Update Untitled2.cpp

--- Untitled2.cpp
+++ Untitled2.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
-unsigned int v = 0;
+unsigned int v = 0; // \/ These two values are added up to create what will be saved into array
 unsigned int A[] = {0,3,3};
 unsigned int B[] = {0,3,3};
 unsigned int l = sizeof(A)/4;
 unsigned int array[] = {0,0,0};
 unsigned int oarray[] = {0,0,0};
 unsigned int base = 10;
-int debugarray(){
+int debugarray(){ //A debug function that prints
 	unsigned int i = 0;
 	while(i<l){
 		cout << array[i];
@@ -51,10 +51,10 @@ int adjust(){
 	unsigned int g=0;
 	while(v<l){
 		debugarray();
-		if(array[v]>9){
-			g=array[v]/base;
-			array[v-1]=array[v-1]+(g);
-			array[v]=array[v]-(g*base);
+		if(array[v]>9){ //It skips the cauculations if it's bigger than nine
+			g=array[v]/base; //I used the / operation to an intenger
+			array[v-1]=array[v-1]+(g); //So tnat 32/10=3
+			array[v]=array[v]-(g*base); //The remainer is 32-(3*10)=2
 		}
 		debugarray();
 		v=v+1;
@@ -63,21 +63,21 @@ int adjust(){
 	displayarray();
 	cout << '\n';
 }
-int add(){
+int add(){ //It adds the two vectors together
 	v=0;
 	while(v<l){
 		array[v]=A[v]+B[v];
 		v=v+1;
 	}
 }
-int setl(){
+int setl(){ //Sets OARRAY to be ARRAY
 	unsigned int i = 0;
 	while(i<l){
 		oarray[i]=array[i];
 		i=i+1;
 	}
 }
-int alllegal(){
+int alllegal(){ //Are all numbers in the vector < base number
 	int i = 0;
 	bool r = 0;
 	while (i<l | r==1){
@@ -87,7 +87,7 @@ int alllegal(){
 	return r;
 	
 }
-int checkarrays(){
+int checkarrays(){ //This checks if the adapted vector and the initial vector are all the same, returning 0, if not, it returns 1
 	int i = 0;
 	bool r = 0;
 	while (i<l | r==1){
@@ -98,8 +98,8 @@ int checkarrays(){
 }
 int main(){
 	add();
-	while(alllegal()/*checkarrays()*/){
-		//setl();
+	while(checkarrays()){ /*You could try to use alllegal() instead of checking if there's no effect is seen when applying the function
+		setl();       and delete this line but that doesn't work in my computer so I'm not sure */
 		adjust();
 	}
 	debugarray();
